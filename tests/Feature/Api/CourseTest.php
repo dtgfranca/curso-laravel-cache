579512a13c0a281ef6463afc540f44b542b9fc6e
Diego França <diego.lightbase@sebraemg.com.br>
2022-08-19 21:53:57 -0300

    test: adiciona os testes para update, delete e select

--- tests/Feature/Api/CourseTest.php
+++ tests/Feature/Api/CourseTest.php
@@ -2,12 +2,16 @@
 
 namespace Tests\Feature\Api;
 
+use App\Models\Course;
+use Database\Factories\CourseFactory;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class CourseTest extends TestCase
 {
+    use DatabaseMigrations;
     /**
      * A basic feature test example.
      *
@@ -15,8 +19,33 @@ class CourseTest extends TestCase
      */
     public function test_should_return_status_code_200_in_all_courses()
     {
-        $response = $this->get('/courses');
+        Course::factory()->count(10)->create();
 
+        $response = $this->get('/courses');
         $response->assertStatus(200);
+    }
+    public function test_should_store_course()
+    {
+        $response = $this->postJson('/courses',[
+            'title'=>fake()->sentence(12),
+            'name'=>fake()->sentence(20),
+            'description'=> fake()->sentence(200)
+        ]);
+        $response->assertStatus(201);
+    }
+    public function test_should_update_course()
+    {
+        $course = Course::factory()->create();
+        $response = $this->putJson('/courses/'.$course->uuid,[
+            'name'=>fake()->sentence(20),
+            'description'=> fake()->sentence(200)
+        ]);
+        $response->assertStatus(200);
+    }
+    public function test_should_delete_course()
+    {
+        $course =  Course::factory()->create();
+        $response = $this->deleteJson('/courses/'.$course->uuid);
+        $response->assertNoContent();
     }
 }
